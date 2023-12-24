b121eda48058bf00951a6f4533d34eabeb953ffa
Esteban Sanchez <exloz@hotmail.com>
2023-12-24 14:26:43 -0500

    Created and added logic for websocket conection of players and integrated chat

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,26 +1,97 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ChatService } from '../../services/chat.service';
-import { ChatMessage } from '../../models/chat-message';
+import { FormsModule } from '@angular/forms';
+import { Client } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
 
 @Component({
     selector: 'app-chat',
     standalone: true,
-    imports: [],
+    imports: [FormsModule, CommonModule],
     templateUrl: './chat.component.html',
     styleUrl: './chat.component.css',
 })
 export class ChatComponent implements OnInit {
-    constructor(private chatService: ChatService) {}
+    private stompClient!: Client;
+    public userName: string = 'Exloz';
+    public messageContent: string = '';
+    public messages: any[] = [];
+    private colors: string[] = [
+        '#2196F3',
+        '#32c787',
+        '#00BCD4',
+        '#ff5652',
+        '#ffc107',
+        '#ff85af',
+        '#FF9800',
+        '#39bbb0',
+    ];
 
     ngOnInit(): void {
-        this.chatService.joinGameSet('ABC');
+        this.connect();
     }
 
-    sendMessage() {
-        const ChatMessage = {
-            message: 'Hola',
-            user: '1',
-        } as ChatMessage;
-        this.chatService.sendMessage('ABC', ChatMessage);
+    connect(): void {
+        const url = '//localhost:8080/chat-socket';
+        const socket = new SockJS(url);
+        this.stompClient = new Client({
+            webSocketFactory: () => socket,
+        });
+        console.log(socket);
+
+        this.stompClient.onConnect = (frame: any) => {
+            this.onConnected(frame);
+        };
+
+        this.stompClient.activate();
+    }
+
+    private onConnected(frame: any): void {
+        this.stompClient.subscribe(
+            '/topic/public',
+            this.onMessageReceived.bind(this)
+        );
+        this.stompClient.publish({
+            destination: '/app/chat.addUser',
+            body: JSON.stringify({ sender: this.userName, type: 'JOIN' }),
+        });
+    }
+
+    sendMessage(): void {
+        if (this.messageContent && this.stompClient.active) {
+            const chatMessage = {
+                sender: this.userName,
+                content: this.messageContent,
+                type: 'CHAT',
+            };
+            this.stompClient.publish({
+                destination: '/app/chat.sendMessage',
+                body: JSON.stringify(chatMessage),
+            });
+            console.log(chatMessage);
+
+            this.messageContent = '';
+        }
+    }
+
+    private onMessageReceived(payload: any): void {
+        const message = JSON.parse(payload.body);
+
+        if (message.type === 'JOIN') {
+            message.content = message.sender + ' joined!';
+        } else if (message.type === 'LEAVE') {
+            message.content = message.sender + ' left!';
+        }
+
+        this.messages.push(message);
+    }
+
+    getAvatarColor(messageSender: string): string {
+        let hash = 0;
+        for (let i = 0; i < messageSender.length; i++) {
+            hash = 31 * hash + messageSender.charCodeAt(i);
+        }
+        const index = Math.abs(hash % this.colors.length);
+        return this.colors[index];
     }
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,38 +1,38 @@
-import { Injectable } from '@angular/core';
-import { Stomp } from '@stomp/stompjs';
-import SockJS from 'sockjs-client';
-import { ChatMessage } from '../models/chat-message';
+// import { Injectable } from '@angular/core';
+// import { Stomp } from '@stomp/stompjs';
+// import SockJS from 'sockjs-client';
+// import { ChatMessage } from '../models/chat-message';
 
-@Injectable({
-    providedIn: 'root',
-})
-export class ChatService {
-    private stompClient: any;
+// @Injectable({
+//     providedIn: 'root',
+// })
+// export class ChatService {
+//     private stompClient: any;
 
-    constructor() {
-        this.initConnectionSocket;
-    }
+//     constructor() {
+//         this.initConnectionSocket;
+//     }
 
-    initConnectionSocket() {
-        const url = '//localhost:8080/chat-socket';
-        const socket = new SockJS(url);
-        this.stompClient = Stomp.over(socket);
-    }
+//     initConnectionSocket() {
+//         const url = '//localhost:8080/chat-socket';
+//         const socket = new SockJS(url);
+//         this.stompClient = Stomp.over(socket);
+//     }
 
-    joinGameSet(roomId: String) {
-        this.stompClient.connect({}, () => {
-            this.stompClient.suscribe(`/topic/${roomId}`, (messages: any) => {
-                const messageContent = JSON.parse(messages.body);
-                console.log(messageContent);
-            });
-        });
-    }
+//     joinGameSet(roomId: String) {
+//         this.stompClient.connect({}, () => {
+//             this.stompClient.suscribe(`/topic/${roomId}`, (messages: any) => {
+//                 const messageContent = JSON.parse(messages.body);
+//                 console.log(messageContent);
+//             });
+//         });
+//     }
 
-    sendMessage(roomId: String, ChatMessage: ChatMessage) {
-        this.stompClient.send(
-            `/app/chat/${roomId}`,
-            {},
-            JSON.stringify(ChatMessage)
-        );
-    }
-}
+//     sendMessage(roomId: String, ChatMessage: ChatMessage) {
+//         this.stompClient.send(
+//             `/app/chat/${roomId}`,
+//             {},
+//             JSON.stringify(ChatMessage)
+//         );
+//     }
+// }
