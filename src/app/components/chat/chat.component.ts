c60197f2702b50034db7d89f4818c6f1d7478815
Esteban Sanchez <exloz@hotmail.com>
2023-12-23 22:22:10 -0500

    Created html and css chat components

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import SockJS from 'sockjs-client';
+import { Client } from '@stomp/stompjs';
 
 @Component({
-  selector: 'app-chat',
-  standalone: true,
-  imports: [],
-  templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+    selector: 'app-chat',
+    standalone: true,
+    imports: [],
+    templateUrl: './chat.component.html',
+    styleUrl: './chat.component.css',
 })
 export class ChatComponent {
+    private stompClient: any = null;
+    public userName: string = '';
+    private colors: string[] = [
+        '#2196F3',
+        '#32c787',
+        '#00BCD4',
+        '#ff5652',
+        '#ffc107',
+        '#ff85af',
+        '#FF9800',
+        '#39bbb0',
+    ];
 
 }
